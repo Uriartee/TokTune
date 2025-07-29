863f770e45d034a65571d5c67a83f60c9c8f4798
Uriarte <alvaro.uriarte10@gmail.com>
2025-07-29 11:49:53 +0200

    TokTune start

---
+++ src/main/java/com/uriarte/toktune/ToktuneApplication.java
@@ -0,0 +1,13 @@
+package com.uriarte.toktune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToktuneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToktuneApplication.class, args);
+	}
+
+}
